6cae1c8dc67e76342982bc12010331a0edc6f830
Mélinda <melindabcci@gmail.com>
2024-01-22 13:12:18 +0100

    wip exercice vue

---
+++ vue/exovuejs-1/src/componentoption.vue
@@ -0,0 +1,14 @@
+<script>
+  const { createApp } = Vue
+  import componentsoption from './assets/main.js'
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        name : 'Marguerite',
+        
+      }
+    }
+  }).mount('#app')
+</script>
--- vue/exovuejs-1/src/main.js
+++ vue/exovuejs-1/src/main.js
@@ -1,20 +1,8 @@
 import './assets/main.css'
-
-
-// import { createApp} from 'vue'
-// import App from './App.vue'
-
-// createApp(App).mount('#app') 
-
 import main from '/src/main.js'
 
-const { createApp, ref} = Vue 
 
-createApp({
-    setup() {
-        const message = ref('Au bout........')
-        return {
-            message
-        }
-    }
-}). mount('#app')
+ import { createApp} from 'vue'
+ import App from './App.vue'
+
+ createApp(App).mount('#app') 
