ad4caa6306cbab8f164587e713046c665fbbaf6f
Mélinda <melindabcci@gmail.com>
2024-01-24 09:15:38 +0100

    wip exo 4 vuejs

---
+++ vue/exovuejs-4/course-4/exercice_3/src/components/Layout/MainLayout.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        name: "MainLayout"
+    }
+</script>
+<template>
+    <!-- redéfinir les slots  -->
+        <!-- - nommé les slots -->
+        <!-- - rendu consitionnel de slots / expliquer -->
+        <!-- - injecter dans le slot Header -->
+    <div class="container">
+        <header
+            v-if="$slots.header"
+            class="p-3 mb-3 border-bottom"
+        >
+            <slot name="header"></slot>
+        </header>
+        <main v-if="$slots.default">
+            <slot></slot>
+        </main>
+        <footer v-if="$slots.footer">
+            <slot
+                name="footer"
+            ></slot>
+        </footer>
+    </div>
+</template>
